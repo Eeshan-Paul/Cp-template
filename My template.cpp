982c50f385bc22ff76912d6cee48bab273a1ced9
Eeshan Paul <62149967+Eeshan-Paul@users.noreply.github.com>
2021-09-26 17:03:46 +0530

    Add files via upload

---
+++ My template.cpp
@@ -0,0 +1,40 @@
+        // Made  by Eeshan Paul
+
+        #include<bits/stdc++.h>
+        using namespace std;
+
+        #define ff              first
+        #define ss              second
+        #define int             long long
+        #define pb              push_back
+        #define pii             pair<int,int>
+        #define vi              vector<int>
+        #define mii             map<int,int>
+        #define pqb             priority_queue<int>
+        #define pqs             priority_queue<int,vi,greater<int> >
+        #define setbits(x)      __builtin_popcountll(x)
+        #define zrobits(x)      __builtin_ctzll(x)
+        #define inf             1e18
+        #define ps(x,y)         fixed<<setprecision(y)<<x
+        #define mk(arr,n,type)  type *arr=new type[n];
+        #define f(x)            int x; cin>>x; while(x--)
+        #define rep(i,a,b)      for (long long i=a;i<b;i++)
+        #define rof(i,a,b)		for (int i=(a-1);i>=b;i--)
+        #define multiply(n)     ((n*(n+1))/2)
+        #define BLOCK           555
+        #define sz(x)           (int)(x).size()
+        mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+      
+        void setIO(string name = "") { 
+	        ios_base::sync_with_stdio(0); cin.tie(0); 
+	        if(sz(name)){
+                freopen((name+".in").c_str(), "r", stdin); 
+                freopen((name+".out").c_str(), "w", stdout);
+            }
+        }   
+
+        int32_t main()
+        {
+            
+            return 0;
+        }
